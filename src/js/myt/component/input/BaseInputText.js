1647fbf42696dd7db542ce44f9c59be97f934517
Maynard Demmon <maynarddemmon@gmail.com>
2013-09-29 22:02:11 -0700

    Added an allowedChars attribute to BaseInputText

--- src/js/myt/component/input/BaseInputText.js
+++ src/js/myt/component/input/BaseInputText.js
@@ -4,6 +4,8 @@
         spellcheck:boolean turns browser spellchecking on and off.
         maxLength:int sets a maximum number of input characters. Set to -1 to
             turn of max length.
+        allowedChars:string each character in the string is an allowed
+            input character. If not set all characters are allowed.
 */
 myt.BaseInputText = new JS.Class('BaseInputText', myt.NativeInputWrapper, {
     include: [myt.TextSupport],
@@ -41,20 +43,33 @@ myt.BaseInputText = new JS.Class('BaseInputText', myt.NativeInputWrapper, {
     },
     
     setMaxLength: function(v) {
-        if (v === null || v === undefined || 0 > v) v = undefined;
+        if (v == null || 0 > v) v = undefined;
         
         if (this.maxLength === v) return;
         this.maxLength = this.domElement.maxLength = v;
         if (this.inited) this.fireNewEvent('maxLength', v);
     },
     
+    setAllowedChars: function(v) {
+        this.allowedChars = v;
+    },
+    
     
     // Methods /////////////////////////////////////////////////////////////////
     _filterInput: function(v) {
-        var de = this.domElement;
-        var curValue = de.value;
-        var newValue = this.filterInput(curValue);
+        var de = this.domElement, curValue = de.value,
+            newValue = this.filterInput(this._filterForAllowedChars(curValue));
         if (curValue !== newValue) de.value = newValue;
+    },
+    
+    _filterForAllowedChars: function(v) {
+        var allowedChars = this.allowedChars;
+        if (allowedChars) {
+            var chars = v.split(''), i = chars.length;
+            while (i) if (allowedChars.indexOf(chars[--i]) === -1) chars.splice(i, 1);
+            v = chars.join('');
+        }
+        return v;
     },
     
     filterInput: function(v) {
@@ -89,8 +104,7 @@ myt.BaseInputText = new JS.Class('BaseInputText', myt.NativeInputWrapper, {
     setCaretPosition: function(start, end) {
         if (end === undefined || start === end) {
             // Don't update if the current position already matches.
-            var curPos = this.getCaretPosition();
-            if (curPos === start) return;
+            if (this.getCaretPosition() === start) return;
             
             end = start;
         }
