ae0c58815ceb07984da6568cc4f438c217481ff6
Maynard Demmon <maynarddemmon@gmail.com>
2014-01-28 17:36:02 -0800

    Optimization of core jsclass that eliminates features that myt does not use. If problems are encountered or jsclass revs we should start fresh since this is a minor opitimization.

--- src/js/jsclass/src/core.js
+++ src/js/jsclass/src/core.js
@@ -15,84 +15,50 @@ var JS = (typeof this.JS === 'undefined') ? {} : this.JS;
 })(function(global, exports) {
 'use strict';
 
-var JS = {ENV: global};
-
-JS.END_WITHOUT_DOT = /([^\.])$/;
-
-JS.array = function(enumerable) {
-  var array = [], i = enumerable.length;
-  while (i--) array[i] = enumerable[i];
-  return array;
-};
-
-JS.bind = function(method, object) {
-  return function() {
-    return method.apply(object, arguments);
-  };
-};
-
-JS.Date = JS.ENV.Date;
-
-JS.extend = function(destination, source, overwrite) {
-  if (!destination || !source) return destination;
-  for (var field in source) {
-    if (destination[field] === source[field]) continue;
-    if (overwrite === false && destination.hasOwnProperty(field)) continue;
-    destination[field] = source[field];
+var JS = {
+  extend: function(destination, source, overwrite) {
+    if (!destination || !source) return destination;
+    for (var field in source) {
+      if (destination[field] === source[field]) continue;
+      if (overwrite === false && destination.hasOwnProperty(field)) continue;
+      destination[field] = source[field];
+    }
+    return destination;
+  },
+  
+  isType: function(object, type) {
+    if (typeof type === 'string') return typeof object === type;
+  
+    if (object === null || object === undefined) return false;
+  
+    return (typeof type === 'function' && object instanceof type) ||
+           (object.isA && object.isA(type)) ||
+           object.constructor === type;
+  },
+  
+  makeBridge: function(parent) {
+    var bridge = function() {};
+    bridge.prototype = parent.prototype;
+    return new bridge();
+  },
+  
+  makeClass: function(parent) {
+    parent = parent || Object;
+  
+    var constructor = function() {
+      return this.initialize
+           ? this.initialize.apply(this, arguments) || this
+           : this;
+    };
+    constructor.prototype = this.makeBridge(parent);
+  
+    constructor.superclass = parent;
+  
+    constructor.subclasses = [];
+    if (parent.subclasses) parent.subclasses.push(constructor);
+  
+    return constructor;
   }
-  return destination;
-};
-
-JS.indexOf = function(list, item) {
-  if (list.indexOf) return list.indexOf(item);
-  var i = list.length;
-  while (i--) {
-    if (list[i] === item) return i;
-  }
-  return -1;
-};
-
-JS.isType = function(object, type) {
-  if (typeof type === 'string')
-    return typeof object === type;
-
-  if (object === null || object === undefined)
-    return false;
-
-  return (typeof type === 'function' && object instanceof type) ||
-         (object.isA && object.isA(type)) ||
-         object.constructor === type;
-};
-
-JS.makeBridge = function(parent) {
-  var bridge = function() {};
-  bridge.prototype = parent.prototype;
-  return new bridge();
-};
-
-JS.makeClass = function(parent) {
-  parent = parent || Object;
-
-  var constructor = function() {
-    return this.initialize
-         ? this.initialize.apply(this, arguments) || this
-         : this;
-  };
-  constructor.prototype = JS.makeBridge(parent);
-
-  constructor.superclass = parent;
-
-  constructor.subclasses = [];
-  if (parent.subclasses) parent.subclasses.push(constructor);
-
-  return constructor;
-};
-
-JS.match = function(category, object) {
-  if (object === undefined) return false;
-  return typeof category.test === 'function'
-       ? category.test(object)
-       : category.match(object);
 };
 
 JS.Method = JS.makeClass();
@@ -102,25 +68,7 @@ JS.extend(JS.Method.prototype, {
     this.module   = module;
     this.name     = name;
     this.callable = callable;
-
-    this._words = {};
-    if (typeof callable !== 'function') return;
-
-    this.arity  = callable.length;
-
-    var matches = callable.toString().match(/\b[a-z\_\$][a-z0-9\_\$]*\b/ig),
-        i       = matches.length;
-
-    while (i--) this._words[matches[i]] = true;
-  },
-
-  setName: function(name) {
-    this.callable.displayName =
-    this.displayName = name;
-  },
-
-  contains: function(word) {
-    return this._words.hasOwnProperty(word);
+    this._hasSuper = typeof callable === 'function' && callable.toString().indexOf('callSuper') !== -1;
   },
 
   call: function() {
@@ -132,66 +80,40 @@ JS.extend(JS.Method.prototype, {
   },
 
   compile: function(environment) {
-    var method     = this,
-        trace      = method.module.__trace__ || environment.__trace__,
-        callable   = method.callable,
-        words      = method._words,
-        allWords   = JS.Method._keywords,
-        i          = allWords.length,
-        keywords   = [],
-        keyword;
+    var method = this,
+        callable = method.callable,
+        keywordCallSuper = JS.Method.keywordCallSuper,
+        superFunc = method._hasSuper && keywordCallSuper ? keywordCallSuper : null;
 
-    while  (i--) {
-      keyword = allWords[i];
-      if (words[keyword.name]) keywords.push(keyword);
-    }
-    if (keywords.length === 0 && !trace) return callable;
-
-    var compiled = function() {
-      var N = keywords.length, j = N, previous = {}, keyword, existing, kwd;
-
-      while (j--) {
-        keyword  = keywords[j];
-        existing = this[keyword.name];
-
-        if (existing && !existing.__kwd__) continue;
-
-        previous[keyword.name] = {
-          _value: existing,
-          _own:   this.hasOwnProperty(keyword.name)
-        };
-        kwd = keyword.filter(method, environment, this, arguments);
+    return superFunc === null ? callable : function() {
+      var prevValue, prevOwn, 
+        existing = this.callSuper,
+        doSuper = !existing || existing.__kwd__;
+      
+      if (doSuper) {
+        prevValue = existing;
+        prevOwn = this.hasOwnProperty('callSuper');
+        var kwd = this.callSuper = superFunc(method, environment, this, arguments);
         if (kwd) kwd.__kwd__ = true;
-        this[keyword.name] = kwd;
       }
-      var returnValue = callable.apply(this, arguments),
-          j = N;
-
-      while (j--) {
-        keyword = keywords[j];
-        if (!previous[keyword.name]) continue;
-        if (previous[keyword.name]._own)
-          this[keyword.name] = previous[keyword.name]._value;
-        else
-          delete this[keyword.name];
+      
+      var returnValue = callable.apply(this, arguments);
+      
+      if (doSuper) {
+        if (prevOwn) {
+          this.callSuper = prevValue;
+        } else {
+          delete this.callSuper;
+        }
       }
+      
       return returnValue;
     };
-
-    var StackTrace = trace && (exports.StackTrace || require('./stack_trace').StackTrace);
-    if (trace) return StackTrace.wrap(compiled, method, environment);
-    return compiled;
-  },
-
-  toString: function() {
-    var name = this.displayName || (this.module.toString() + '#' + this.name);
-    return '#<Method:' + name + '>';
   }
 });
 
 JS.Method.create = function(module, name, callable) {
-  if (callable && callable.__inc__ && callable.__fns__)
-    return callable;
+  if (callable && callable.__inc__ && callable.__fns__) return callable;
 
   var method = (typeof callable !== 'function')
              ? callable
@@ -202,9 +124,7 @@ JS.Method.create = function(module, name, callable) {
 };
 
 JS.Method.compile = function(method, environment) {
-  return (method instanceof this)
-      ? method.compile(environment)
-      : method;
+  return (method instanceof this) ? method.compile(environment) : method;
 };
 
 JS.Method.__listeners__ = [];
@@ -218,47 +138,9 @@ JS.Method.notify = function(method) {
       i = listeners.length,
       listener;
 
-  while (i--) {
-    listener = listeners[i];
+  while (i) {
+    listener = listeners[--i];
     listener[0].call(listener[1], method);
-  }
-};
-
-JS.Method._keywords = [];
-
-JS.Method.keyword = function(name, filter) {
-  this._keywords.push({name: name, filter: filter});
-};
-
-JS.Method.tracing = function(classes, block, context) {
-  var pkg = exports.require ? exports : require('./loader');
-  pkg.require('JS.StackTrace', function(StackTrace) {
-    var logger = StackTrace.logger,
-        active = logger.active;
-
-    classes = [].concat(classes);
-    this.trace(classes);
-    logger.active = true;
-    block.call(context);
-
-    this.untrace(classes);
-    logger.active = active;
-  }, this);
-};
-
-JS.Method.trace = function(classes) {
-  var i = classes.length;
-  while (i--) {
-    classes[i].__trace__ = true;
-    classes[i].resolve();
-  }
-};
-
-JS.Method.untrace = function(classes) {
-  var i = classes.length;
-  while (i--) {
-    classes[i].__trace__ = false;
-    classes[i].resolve();
   }
 };
 
@@ -281,32 +163,10 @@ JS.extend(JS.Module.prototype, {
     this.__anc__ = null;
     this.__mct__ = {};
 
-    this.setName(name);
     this.include(methods, {_resolve: false});
 
-    if (JS.Module.__queue__)
-      JS.Module.__queue__.push(this);
-  },
-
-  setName: function(name) {
-    this.displayName = name || '';
-
-    for (var field in this.__fns__)
-      this.__name__(field);
-
-    if (name && this.__meta__)
-      this.__meta__.setName(name + '.');
-  },
-
-  __name__: function(name) {
-    if (!this.displayName) return;
-
-    var object = this.__fns__[name];
-    if (!object) return;
-
-    name = this.displayName.replace(JS.END_WITHOUT_DOT, '$1#') + name;
-    if (typeof object.setName === 'function') return object.setName(name);
-    if (typeof object === 'function') object.displayName = name;
+    var queue = JS.Module.__queue__;
+    if (queue) queue.push(this);
   },
 
   define: function(name, callable, options) {
@@ -314,7 +174,6 @@ JS.extend(JS.Module.prototype, {
         resolve = (options || {})._resolve;
 
     this.__fns__[name] = method;
-    this.__name__(name);
     if (resolve !== false) this.resolve();
   },
 
@@ -332,45 +191,30 @@ JS.extend(JS.Module.prototype, {
       if ((module.__dep__ || {}).push) module.__dep__.push(this);
 
       if (extended = options._extended) {
-        if (typeof module.extended === 'function')
-          module.extended(extended);
+        if (typeof module.extended === 'function') module.extended(extended);
+      } else {
+        if (typeof module.included === 'function') module.included(this);
       }
-      else {
-        if (typeof module.included === 'function')
-          module.included(this);
-      }
-    }
-    else {
+    } else {
       if (this.shouldIgnore('extend', extend)) {
         mixins = [].concat(extend);
-        for (i = 0, n = mixins.length; i < n; i++)
-          this.extend(mixins[i]);
+        for (i = 0, n = mixins.length; i < n;) this.extend(mixins[i++]);
       }
       if (this.shouldIgnore('include', include)) {
         mixins = [].concat(include);
-        for (i = 0, n = mixins.length; i < n; i++)
-          this.include(mixins[i], {_resolve: false});
+        for (i = 0, n = mixins.length; i < n;) this.include(mixins[i++], {_resolve: false});
       }
       for (field in module) {
-        if (!module.hasOwnProperty(field)) continue;
-        value = module[field];
-        if (this.shouldIgnore(field, value)) continue;
-        this.define(field, value, {_resolve: false});
+        if (module.hasOwnProperty(field)) {
+          value = module[field];
+          if (this.shouldIgnore(field, value)) continue;
+          this.define(field, value, {_resolve: false});
+        }
       }
-      if (module.hasOwnProperty('toString'))
-        this.define('toString', module.toString, {_resolve: false});
     }
 
     if (resolve) this.resolve();
     return this;
-  },
-
-  alias: function(aliases) {
-    for (var method in aliases) {
-      if (!aliases.hasOwnProperty(method)) continue;
-      this.define(method, this.instanceMethod(aliases[method]), {_resolve: false});
-    }
-    this.resolve();
   },
 
   resolve: function(host) {
@@ -384,20 +228,17 @@ JS.extend(JS.Module.prototype, {
       this.__anc__ = null;
       this.__mct__ = {};
       i = this.__dep__.length;
-      while (i--) this.__dep__[i].resolve();
+      while (i) this.__dep__[--i].resolve();
     }
 
-    if (!target) return;
-
-    for (i = 0, n = inc.length; i < n; i++)
-      inc[i].resolve(host);
-
-    for (key in fns) {
-      compiled = JS.Method.compile(fns[key], host);
-      if (target[key] !== compiled) target[key] = compiled;
+    if (target) {
+      for (i = 0, n = inc.length; i < n;) inc[i++].resolve(host);
+      
+      for (key in fns) {
+        compiled = JS.Method.compile(fns[key], host);
+        if (target[key] !== compiled) target[key] = compiled;
+      }
     }
-    if (fns.hasOwnProperty('toString'))
-      target.toString = JS.Method.compile(fns.toString, host);
   },
 
   shouldIgnore: function(field, value) {
@@ -413,11 +254,9 @@ JS.extend(JS.Module.prototype, {
 
     if (cachable && this.__anc__) return this.__anc__.slice();
 
-    for (var i = 0, n = inc.length; i < n; i++)
-      inc[i].ancestors(list);
+    for (var i = 0, n = inc.length; i < n;) inc[i++].ancestors(list);
 
-    if (JS.indexOf(list, this) < 0)
-      list.push(this);
+    if (list.indexOf(this) < 0) list.push(this);
 
     if (cachable) this.__anc__ = list.slice();
     return list;
@@ -431,8 +270,8 @@ JS.extend(JS.Module.prototype, {
         methods   = [],
         fns;
 
-    for (var i = 0, n = ancestors.length; i < n; i++) {
-      fns = ancestors[i].__fns__;
+    for (var i = 0, n = ancestors.length; i < n;) {
+      fns = ancestors[i++].__fns__;
       if (fns.hasOwnProperty(name)) methods.push(fns[name]);
     }
     this.__mct__[name] = methods.slice();
@@ -442,11 +281,10 @@ JS.extend(JS.Module.prototype, {
   includes: function(module) {
     if (module === this) return true;
 
-    var inc  = this.__inc__;
+    var inc = this.__inc__;
 
-    for (var i = 0, n = inc.length; i < n; i++) {
-      if (inc[i].includes(module))
-        return true;
+    for (var i = 0, n = inc.length; i < n;) {
+      if (inc[i++].includes(module)) return true;
     }
     return false;
   },
@@ -461,32 +299,27 @@ JS.extend(JS.Module.prototype, {
         field;
 
     for (field in fns) {
-      if (!JS.isType(this.__fns__[field], JS.Method)) continue;
-      if (JS.indexOf(methods, field) >= 0) continue;
-      methods.push(field);
+      if (JS.isType(this.__fns__[field], JS.Method)) {
+        if (methods.indexOf(field) === -1) methods.push(field);
+      }
     }
 
     if (recursive !== false) {
       var ancestors = this.ancestors(), i = ancestors.length;
-      while (i--) ancestors[i].instanceMethods(false, methods);
+      while (i) ancestors[--i].instanceMethods(false, methods);
     }
     return methods;
   },
 
   match: function(object) {
     return object && object.isA && object.isA(this);
-  },
-
-  toString: function() {
-    return this.displayName;
   }
 });
 
 JS.Kernel = new JS.Module('Kernel', {
   __eigen__: function() {
     if (this.__meta__) return this.__meta__;
-    var name = this.toString() + '.';
-    this.__meta__ = new JS.Module(name, null, {_target: this});
+    this.__meta__ = new JS.Module('', null, {_target: this});
     return this.__meta__.include(this.klass, {_resolve: false});
   },
 
@@ -517,24 +350,13 @@ JS.Kernel = new JS.Module('Kernel', {
     if (typeof field !== 'function') return field;
     if (value && field === value._value) return value._bound;
 
-    var bound = JS.bind(field, this);
+    var bound = field.bind(this);
     cache[name] = {_value: field, _bound: bound};
     return bound;
   },
 
   methods: function() {
     return this.__eigen__().instanceMethods();
-  },
-
-  tap: function(block, context) {
-    block.call(context, this);
-    return this;
-  },
-
-  toString: function() {
-    if (this.displayName) return this.displayName;
-    var name = this.klass.displayName || this.klass.toString();
-    return '#<' + name + ':' + this.hash() + '>';
   }
 });
 
@@ -543,7 +365,7 @@ JS.Kernel = new JS.Module('Kernel', {
 
   JS.Kernel.hashFor = function(object) {
     if (object.__hash__ !== undefined) return object.__hash__;
-    object.__hash__ = (new JS.Date().getTime() + id).toString(16);
+    object.__hash__ = (Date.now() + id).toString(16);
     id += 1;
     return object.__hash__;
   };
@@ -574,7 +396,6 @@ JS.extend(JS.Class.prototype, {
     klass.prototype.klass = klass;
 
     klass.__eigen__().include(parent.__meta__, {_resolve: options._resolve});
-    klass.setName(name);
 
     klass.__tgt__ = klass.prototype;
 
@@ -598,11 +419,13 @@ JS.extend(JS.Class.prototype, {
 (function() {
   var methodsFromPrototype = function(klass) {
     var methods = {},
-        proto   = klass.prototype;
+        proto = klass.prototype,
+        field;
 
-    for (var field in proto) {
-      if (!proto.hasOwnProperty(field)) continue;
-      methods[field] = JS.Method.create(klass, field, proto[field]);
+    for (field in proto) {
+      if (proto.hasOwnProperty(field)) {
+        methods[field] = JS.Method.create(klass, field, proto[field]);
+      }
     }
     return methods;
   };
@@ -616,12 +439,10 @@ JS.extend(JS.Class.prototype, {
     klass.__fns__ = methodsFromPrototype(klass);
     klass.__tgt__ = klass.prototype;
 
-    klass.prototype.constructor =
-    klass.prototype.klass = klass;
+    klass.prototype.constructor = klass.prototype.klass = klass;
 
     JS.extend(klass, JS.Class.prototype);
     klass.include(parent || JS.Kernel);
-    klass.setName(name);
 
     klass.constructor = klass.klass = JS.Class;
   };
@@ -637,18 +458,16 @@ JS.extend(JS.Class.prototype, {
   eigen.call(JS.Class).include(JS.Module.__meta__);
 })();
 
-JS.NotImplementedError = new JS.Class('NotImplementedError', Error);
-
-JS.Method.keyword('callSuper', function(method, env, receiver, args) {
+JS.Method.keywordCallSuper = function(method, env, receiver, args) {
   var methods    = env.lookup(method.name),
       stackIndex = methods.length - 1,
-      params     = JS.array(args);
+      params     = Array.prototype.slice.call(args);
 
   if (stackIndex === 0) return undefined;
 
   var _super = function() {
     var i = arguments.length;
-    while (i--) params[i] = arguments[i];
+    while (i) params[--i] = arguments[i];
 
     stackIndex -= 1;
     if (stackIndex === 0) delete receiver.callSuper;
@@ -660,46 +479,7 @@ JS.Method.keyword('callSuper', function(method, env, receiver, args) {
   };
 
   return _super;
-});
-
-JS.Method.keyword('blockGiven', function(method, env, receiver, args) {
-  var block = Array.prototype.slice.call(args, method.arity),
-      hasBlock = (typeof block[0] === 'function');
-
-  return function() { return hasBlock };
-});
-
-JS.Method.keyword('yieldWith', function(method, env, receiver, args) {
-  var block = Array.prototype.slice.call(args, method.arity);
-
-  return function() {
-    if (typeof block[0] !== 'function') return;
-    return block[0].apply(block[1] || null, arguments);
-  };
-});
-
-JS.Interface = new JS.Class('Interface', {
-  initialize: function(methods) {
-    this.test = function(object, returnName) {
-      var n = methods.length;
-      while (n--) {
-        if (typeof object[methods[n]] !== 'function')
-          return returnName ? methods[n] : false;
-      }
-      return true;
-    };
-  },
-
-  extend: {
-    ensure: function() {
-      var args = JS.array(arguments), object = args.shift(), face, result;
-      while (face = args.shift()) {
-        result = face.test(object, true);
-        if (result !== true) throw new Error('object does not implement ' + result + '()');
-      }
-    }
-  }
-});
+};
 
 JS.Singleton = new JS.Class('Singleton', {
   initialize: function(name, parent, methods) {
