55cc127d0068357ba571f8b7ca88da5d3932eda2
Maynard Demmon <maynarddemmon@gmail.com>
2014-11-03 19:29:49 -0800

    Initial commit for a gh-pages branch.

---
+++ src/js/boilerplate.js
@@ -0,0 +1,6 @@
+var global = window; // Node.js compatibility.
+global.ROOT = global.ROOT || '../src/js/';
+global.MYT_ROOT = global.ROOT + 'myt/';
+global.MYT_IMAGE_ROOT = global.MYT_ROOT;
+
+JSCLASS_PATH = global.ROOT + 'jsclass/min';
