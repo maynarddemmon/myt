54e9c24c295c33d38ecf0e51205d52b8779a7398
Maynard Demmon <maynarddemmon@gmail.com>
2017-04-12 23:04:13 -0700

    Added simple bash scripts to make building the dist related files quick and easy.

---
+++ bin/build_loader.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+# It is expected that you will run this from the directory above this one.
+java -Xmx256m -jar bin/closure-compiler/compiler.jar --js src/js/jsclass/loader-browser.js --js_output_file dist/loader-browser.min.js --source_map_format=V3 --create_source_map dist/loader-browser.min.js.map --output_wrapper "%output%
+//# sourceMappingURL=./loader-browser.min.js.map"
