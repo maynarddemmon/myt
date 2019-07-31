428dc6f4b3b329b3d55bc94ca473fda5ab848d8a
Maynard Demmon <maynard@joingo.com>
2019-07-31 13:41:16 -0700

    Date shim test was broken when run on the 31st of the month.

--- src/test/tests/shim/test-language.js
+++ src/test/tests/shim/test-language.js
@@ -4,10 +4,10 @@ test("Date.format API exists.", function() {
     ok(typeof Date.prototype.format === "function", "Date should have a format function.");
     var date = new Date();
     date.setYear(2015);
-    date.setMonth(3); // April
     date.setDate(9);
+    date.setMonth(3); // April
     date.setHours(16);
     date.setMinutes(30);
     date.setSeconds(45);
-    ok(date.format("Y-m-d H:i:s") === '2015-04-09 16:30:45', "Format a date.");
+    ok(date.format("Y-m-d H:i:s") === '2015-04-09 16:30:45', "Format a date failed: " + date.format("Y-m-d H:i:s"));
 });
