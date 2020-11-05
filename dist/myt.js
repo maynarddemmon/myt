54873c7939777867c7fb891b94b57787a29105c0
Maynard Demmon <maynard@joingo.com>
2020-11-05 12:19:35 -0800

    Prevent Dimmer restore focus code on hide code from running if the Dimmer is not in fact being hidden.

--- dist/myt.js
+++ dist/myt.js
@@ -17771,117 +17771,117 @@ myt.ImageUploader = new JS.Class('ImageUploader', myt.Uploader, {
 })(window, jQuery);
 
 
-/** A dimmer that can be placed on another myt.View to obscure the subviews of
-    that view.
+((pkg) => {
+    const SizeToParent = pkg.SizeToParent;
     
-    Events:
-        None
-    
-    Attributes:
-        restoreFocus:boolean when true focus will be sent back to the view
-            that had focus before the dimmer was shown when the dimmer is
-            hidden. Defaults to true.
-        prevFocus:myt.View or dom element. The thing to set focus on when
-            the dimmer is hidden if restoreFocus is true.
-*/
-myt.Dimmer = new JS.Class('Dimmer', myt.View, {
-    include: [myt.SizeToParent],
-    
-    
-    // Class Methods and Attributes ////////////////////////////////////////////
-    extend: {
-        DEFAULT_OPACITY: 0.35,
-        DEFAULT_COLOR: '#000000'
-    },
-    
-    
-    // Life Cycle //////////////////////////////////////////////////////////////
-    /** @overrides myt.View */
-    initNode: function(parent, attrs) {
-        const self = this;
+    /** A dimmer that can be placed on another myt.View to obscure the subviews
+        of that view.
         
-        self.restoreFocus = true;
+        Events:
+            None
         
-        attrs.focusable = attrs.focusCage = true;
+        Attributes:
+            restoreFocus:boolean when true focus will be sent back to the view
+                that had focus before the dimmer was shown when the dimmer is
+                hidden. Defaults to true.
+            prevFocus:myt.View or dom element. The thing to set focus on when
+                the dimmer is hidden if restoreFocus is true.
         
-        if (attrs.percentOfParentWidth == null) attrs.percentOfParentWidth = 100;
-        if (attrs.percentOfParentHeight == null) attrs.percentOfParentHeight = 100;
-        if (attrs.visible == null) attrs.visible = false;
-        if (attrs.ignoreLayout == null) attrs.ignoreLayout = true;
+        @class */
+    pkg.Dimmer = new JS.Class('Dimmer', pkg.View, {
+        include: [SizeToParent],
         
-        self.callSuper(parent, attrs);
         
-        // Eat mouse events
-        self.attachDomObserver(self, 'eatMouseEvent', 'mouseover');
-        self.attachDomObserver(self, 'eatMouseEvent', 'mouseout');
-        self.attachDomObserver(self, 'eatMouseEvent', 'mousedown');
-        self.attachDomObserver(self, 'eatMouseEvent', 'mouseup');
-        self.attachDomObserver(self, 'eatMouseEvent', 'click');
-        self.attachDomObserver(self, 'eatMouseEvent', 'dblclick');
-        self.attachDomObserver(self, 'eatMouseEvent', 'mousemove');
+        // Class Methods and Attributes ////////////////////////////////////////
+        extend: {
+            DEFAULT_OPACITY: 0.35,
+            DEFAULT_COLOR: '#000000'
+        },
         
-        myt.RootView.setupCaptureDrop(self);
-    },
-    
-    /** @overrides myt.View */
-    doBeforeAdoption: function() {
-        this.callSuper();
         
-        const M = myt,
-            D = M.Dimmer;
-        new M.View(this, {
-            name:'overlay', ignorePlacement:true, 
-            opacity:D.DEFAULT_OPACITY,
-            bgColor:D.DEFAULT_COLOR,
-            percentOfParentWidth:100,
-            percentOfParentHeight:100
-        }, [M.SizeToParent]);
-    },
-    
-    /** @overrides myt.View */
-    destroyAfterOrphaning: function() {
-        myt.RootView.teardownCaptureDrop(this);
+        // Life Cycle //////////////////////////////////////////////////////////
+        /** @overrides myt.View */
+        initNode: function(parent, attrs) {
+            const self = this;
+            
+            self.restoreFocus = true;
+            
+            attrs.focusable = attrs.focusCage = true;
+            
+            if (attrs.percentOfParentWidth == null) attrs.percentOfParentWidth = 100;
+            if (attrs.percentOfParentHeight == null) attrs.percentOfParentHeight = 100;
+            if (attrs.visible == null) attrs.visible = false;
+            if (attrs.ignoreLayout == null) attrs.ignoreLayout = true;
+            
+            self.callSuper(parent, attrs);
+            
+            // Eat mouse events
+            ['mouseover','mouseout','mousedown','mouseup','click','dblclick','mousemove'].forEach((eventName) => {self.attachDomObserver(self, 'eatMouseEvent', eventName);});
+            
+            pkg.RootView.setupCaptureDrop(self);
+        },
         
-        this.callSuper();
-    },
-    
-    
-    // Accessors ///////////////////////////////////////////////////////////////
-    setRestoreFocus: function(v) {this.restoreFocus = v;},
-    
-    
-    // Methods /////////////////////////////////////////////////////////////////
-    /** A handler for mouse events that does nothing and prevents propogation.
-        @param {!Object} event
-        @return boolean True so that the dom event gets eaten. */
-    eatMouseEvent: function(event) {
-        return true;
-    },
-    
-    /** Shows the dimmer and remembers the focus location.
-        @returns {undefined} */
-    show: function() {
-        const self = this,
-            gf = myt.global.focus;
-        self.prevFocus = gf.focusedView || gf.focusedDom;
+        /** @overrides myt.View */
+        doBeforeAdoption: function() {
+            this.callSuper();
+            
+            const Dimmer = pkg.Dimmer;
+            new pkg.View(this, {
+                name:'overlay',
+                ignorePlacement:true, 
+                opacity:Dimmer.DEFAULT_OPACITY,
+                bgColor:Dimmer.DEFAULT_COLOR,
+                percentOfParentWidth:100,
+                percentOfParentHeight:100
+            }, [SizeToParent]);
+        },
         
-        self.makeHighestZIndex();
+        /** @overrides myt.View */
+        destroyAfterOrphaning: function() {
+            pkg.RootView.teardownCaptureDrop(this);
+            
+            this.callSuper();
+        },
         
-        // Prevent focus traversing
-        if (self.focusable) self.focus();
         
-        self.setVisible(true);
-    },
-    
-    /** Hides the dimmer and restores focus if necessary.
-        @param {boolean} [ignoreRestoreFocus] - When true focus will not be restored.
-        @returns {undefined} */
-    hide: function(ignoreRestoreFocus) {
-        this.setVisible(false);
+        // Accessors ///////////////////////////////////////////////////////////
+        setRestoreFocus: function(v) {this.restoreFocus = v;},
         
-        if (!ignoreRestoreFocus && this.restoreFocus && this.prevFocus) this.prevFocus.focus();
-    }
-});
+        
+        // Methods /////////////////////////////////////////////////////////////
+        /** A handler for mouse events that does nothing and prevents propogation.
+            @param {!Object} event
+            @return boolean True so that the dom event gets eaten. */
+        eatMouseEvent: (event) => true,
+        
+        /** Shows the dimmer and remembers the focus location.
+            @returns {undefined} */
+        show: function() {
+            const self = this,
+                gf = pkg.global.focus;
+            self.prevFocus = gf.focusedView || gf.focusedDom;
+            
+            self.makeHighestZIndex();
+            
+            // Prevent focus traversing
+            if (self.focusable) self.focus();
+            
+            self.setVisible(true);
+        },
+        
+        /** Hides the dimmer and restores focus if necessary.
+            @param {boolean} [ignoreRestoreFocus] - When true focus will not be restored.
+            @returns {undefined} */
+        hide: function(ignoreRestoreFocus) {
+            const self = this;
+            if (self.visible) {
+                self.setVisible(false);
+                
+                if (!ignoreRestoreFocus && self.restoreFocus && self.prevFocus) self.prevFocus.focus();
+            }
+        }
+    });
+})(myt);
 
 
 /** A special "layout" that resizes the parent to fit the children rather than
