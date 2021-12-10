45cce6f7f4805ccf8633cf30cc9d47fb957d98dd
Maynard Demmon <maynard@joingo.com>
2021-12-10 12:10:28 -0800

    Upgrade closure compiler to latest and use ECMASCRIPT_2021 as the compilation target.

--- bin/build_loader.sh
+++ bin/build_loader.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 # It is expected that you will run this from the directory above this one.
-java -Xmx256m -jar bin/closure-compiler/compiler.jar --js src/js/jsclass/loader-browser.js --language_out ECMASCRIPT_2015 --jscomp_warning=lintChecks --js_output_file dist/loader-browser.min.js --source_map_format=V3 --create_source_map dist/loader-browser.min.js.map --output_wrapper "%output%
+java -Xmx256m -jar bin/closure-compiler/compiler_v20211201.jar --js src/js/jsclass/loader-browser.js --language_out ECMASCRIPT_2021 --jscomp_warning=lintChecks --js_output_file dist/loader-browser.min.js --source_map_format=V3 --create_source_map dist/loader-browser.min.js.map --output_wrapper "%output%
 //# sourceMappingURL=./loader-browser.min.js.map"
 
 stat -f%z ./dist/loader-browser.min.js
