115adf8df935f8bc7399339fd04a7519129a901a
Maynard Demmon <maynard@joingo.com>
2024-12-02 18:33:48 -0800

    Simplify the code related to manifest file resolution. Both runtime and compile time.

--- bin/package.js
+++ bin/package.js
@@ -1,221 +1,50 @@
 ((global, exports) => {
-    // Package /////////////////////////////////////////////////////////////////
-    let autoIncrement = 1;
-    
     const HTTP_REGEX = /^https?:/i,
         
-        // Ordered list of unique elements, for storing dependencies
-        OrderedSet = function(initialList) {
+        pkgByName = {},
+        
+        Package = exports.Package = function(paths) {
             const self = this,
-                index = {},
-                list = self.list = [],
-                push = self.push = (...items) => {
-                    items.forEach(item => {
-                        const key = item.id ?? item;
-                        if (!index.hasOwnProperty(key)) {
-                            index[key] = true;
-                            list.push(item);
-                        }
-                    });
-                };
-            
-            if (initialList) push(...initialList);
-        },
-        
-        indexByName = {},
-        
-        getFromCache = name => indexByName[name] ??= {},
-        
-        getObject = name => {
-            if (typeof name === 'string') {
-                const cached = getFromCache(name);
-                if (cached.obj !== undefined) return cached.obj;
-                
-                const parts = name.split('.');
-                
-                let object = global,
-                    part;
-                while (part = parts.shift()) object = object && object[part];
-                
-                return cached.obj = object;
-            }
-        },
-        
-        getByName = name => typeof name === 'string' ? getFromCache(name)?.pkg : name,
-        
-        when = (eventTable, block) => {
-            const eventList = [],
-                objects = {};
-            for (const event in eventTable) {
-                if (eventTable.hasOwnProperty(event)) {
-                    objects[event] = [];
-                    const packageList = (new OrderedSet(eventTable[event])).list;
-                    let i = packageList.length;
-                    while (i--) eventList.push([event, packageList[i], i]);
-                }
-            }
-            
-            let waiting = eventList.length,
-                i = waiting;
-            if (waiting === 0) return block?.(objects);
-            
-            while (i--) {
-                const [eventName, objName, idx] = eventList[i],
-                    pkg = getByName(objName);
-                
-                if (!pkg) throw new Error(objName + ' required to exist');
-                
-                // Event dispatch, for communication between packages
-                const eventCallback = () => {
-                    objects[eventType][idx] = getObject(objName);
-                    if (--waiting === 0) block?.(objects);
-                };
-                if (pkg._events[eventType]) {
-                    eventCallback();
-                } else {
-                    (pkg._observers[eventType] ??= []).push(eventCallback);
-                    
-                    // Load
-                    if (fire(pkg, 'request')) {
-                        const loader = pkg._loader,
-                            deps = pkg._deps.list,
-                            source = pkg._source ?? [];
-                        let n = loader.length;
-                        
-                        when({complete:deps}, () => {
-                            when({complete:deps, load:[pkg]}, () => {fire(pkg, 'complete');});
-                            
-                            if (isLoaded(pkg)) {
-                                fire(pkg, 'load');
-                            } else {
-                                const loadNext = () => {
-                                    if (n === 0) {
-                                        isLoaded(pkg);
-                                        fire(pkg, 'load');
-                                    } else {
-                                        const index = loader.length - n;
-                                        n--;
-                                        loadFile(loader[index], loadNext, source[index]);
-                                    }
-                                };
-                                loadNext();
-                            }
-                        });
-                    }
-                }
-            }
-        },
-        
-        fire = (pkg, eventType) => {
-            const events = pkg._events,
-                observers = pkg._observers;
-            if (events[eventType]) {
-                return false;
-            } else {
-                events[eventType] = true;
-                const observersForEvent = observers[eventType];
-                if (observersForEvent) {
-                    delete observers[eventType];
-                    observersForEvent.forEach(callback => {callback();});
-                }
-                return true;
-            }
-        },
-        
-        isLoaded = pkg => {
-            if (pkg._isLoaded === undefined) {
-                const names = pkg._names.list;
-                let i = names.length;
-                while (i--) {
-                    if (getObject(names[i]) === undefined) return pkg._isLoaded = false;
-                }
-                return pkg._isLoaded = true;
-            } else {
-                return pkg._isLoaded;
-            }
-        },
-        
-        Package = exports.Package = function(loader) {
-            const self = this,
-                names = self._names = new OrderedSet(),
-                deps = self._deps = new OrderedSet();
-            
-            self.id = autoIncrement++;
-            self._loader = loader;
-            self._observers = {};
-            self._events = {};
+                deps = self._deps = new Set();
+            self._paths = paths;
             
             // Functions found in manifest files
             self.provides = (...args) => {
-                args.forEach(item => {getFromCache(item).pkg = self;});
-                names.push(...args);
+                for (const name of args) pkgByName[name] = self;
                 return self;
             };
-            
             self.requires = (...args) => {
-                deps.push(...args);
+                for (const name of args) deps.add(name);
                 return self;
             };
         };
     
-    // Class Functions for Package /////////////////////////////////////////////
     Package.ENV = exports.ENV = global;
-    
-    Package.extractFilePaths = include => {
-        // Creates the list of packages needed for the include list. Resolves the .requires() 
-        // information so that the resulting packages list is in dependency order.
-        const packages = [],
-            includedFiles = [],
-            expand = name => {
-                const pkg = getByName(name);
-                pkg._deps.list.forEach(p => {expand(p);});
-                if (!packages.includes(pkg)) packages.push(pkg);
+    Package.extractFilePaths = names => {
+        const packages = new Set(),
+            expand = nameOrPkg => {
+                const pkg = typeof nameOrPkg === 'string' ? pkgByName[nameOrPkg] : nameOrPkg;
+                for (const dep of pkg._deps) expand(dep);
+                packages.add(pkg);
             };
-        include.forEach(p => {expand(p);});
+        for (const name of names) expand(name);
         
         // Extract the file paths out of the packages list. This is necessary because each file() 
         // declaration in the manifest file can declare more than one file.
-        packages.forEach(pkg => {
-            let paths = pkg._loader;
-            if (!(paths instanceof Array)) throw new Error('Cannot bundle ' + pkg + ': no path specified in your manifest');
-            paths = paths.filter(p => !HTTP_REGEX.test(p));
-            paths.forEach(loader => {includedFiles.push(loader)});
-        });
-        
+        const includedFiles = [];
+        for (const pkg of packages) {
+            if (!Array.isArray(pkg._paths)) throw new Error('Cannot bundle ' + pkg + ': no path specified in your manifest');
+            for (const path of pkg._paths) {
+                if (!HTTP_REGEX.test(path)) includedFiles.push(path);
+            }
+        };
         return includedFiles;
     };
     
-    // File Loader /////////////////////////////////////////////////////////////
-    const loadFile = (path, fireCallbacks) => {
-        let file,
-            module;
-        
-        if (typeof process !== 'undefined') {
-            module = path.replace(/\.[^\.]+$/g, '');
-            file   = require('path').resolve(module);
-        } else if (typeof phantom !== 'undefined') {
-            file = phantom.libraryPath.replace(/\/$/, '') + '/' +
-            path.replace(/^\//, '');
-        }
-        
-        module = require(file);
-        fireCallbacks(module);
-        
-        return module;
-    };
-    
-    // Exports /////////////////////////////////////////////////////////////////
     exports.Packages = manifestFunc => {
         manifestFunc(
             // The "file" function used inside the manifestFunc.
             (...args) => new Package(args.map(filename => !HTTP_REGEX.test(filename) && exports.ROOT ? exports.ROOT + '/' + filename : filename))
         );
-    };
-    
-    exports.require = (...args) => {
-        const files = [];
-        let i = 0;
-        while (typeof args[i] === 'string') files.push(args[i++]);
-        when({complete:files}, args[i]);
     };
 })(global, exports);
--- src/js/jsclass/loader-browser.js
+++ src/js/jsclass/loader-browser.js
@@ -1,117 +1,107 @@
 ((global, exports) => {
-    // Package /////////////////////////////////////////////////////////////////
-    let autoIncrement = 1;
-    
     const HTTP_REGEX = /^https?:/i,
         
-        // Ordered list of unique elements, for storing dependencies
-        OrderedSet = function(initialList) {
-            const self = this,
-                index = {},
-                list = self.list = [],
-                push = self.push = (...items) => {
-                    items.forEach(item => {
-                        const key = item.id ?? item;
-                        if (!index.hasOwnProperty(key)) {
-                            index[key] = true;
-                            list.push(item);
-                        }
-                    });
-                };
-            
-            if (initialList) push(...initialList);
+        pkgByName = {}, // Each entry will be: <name>:{pkg:<Package>, obj:Object}
+        getPkgByName = name => pkgByName[name] ??= {},
+        
+        getPkgObject = name => {
+            if (typeof name === 'string') {
+                const pkg = getPkgByName(name);
+                if (pkg.obj === undefined) {
+                    const parts = name.split('.');
+                    let object = global,
+                        part;
+                    while (part = parts.shift()) object = object && object[part];
+                    pkg.obj = object;
+                }
+                return pkg.obj;
+            }
         },
         
-        // Deferred values used with fetchFile
         Deferred = function() {
-            const self = this,
-                callbacks = [];
+            const callbacks = [];
             let succeeded,
                 value;
-            self.callback = callbackFunc => {
+            this.callback = callbackFunc => {
                 if (succeeded) {
                     callbackFunc(value);
                 } else {
                     callbacks.push(callbackFunc);
                 }
             };
-            self.succeed = v => {
+            this.succeed = v => {
                 succeeded = true;
                 value = v;
                 while (callbacks.length) callbacks.shift()(v);
             };
         },
         
-        indexByName = {},
-        
-        getFromCache = name => indexByName[name] ??= {},
-        
-        getObject = name => {
-            if (typeof name === 'string') {
-                const cached = getFromCache(name);
-                if (cached.obj !== undefined) return cached.obj;
-                
-                const parts = name.split('.');
-                
-                let object = global,
-                    part;
-                while (part = parts.shift()) object = object && object[part];
-                
-                return cached.obj = object;
-            }
-        },
-        
         when = (eventTable, block) => {
             const eventList = [],
                 objects = {};
             for (const event in eventTable) {
-                if (eventTable.hasOwnProperty(event)) {
-                    objects[event] = [];
-                    const packageList = (new OrderedSet(eventTable[event])).list;
-                    let i = packageList.length;
-                    while (i--) eventList.push([event, packageList[i], i]);
-                }
+                objects[event] = [];
+                const packageList = new Set(eventTable[event]);
+                let i = packageList.size;
+                for (const item of packageList) eventList.unshift([event, item, --i]);
             }
             
-            let waiting = eventList.length,
-                i = waiting;
+            let waiting = eventList.length;
             if (waiting === 0) return block?.(objects);
+            let i = waiting;
             
             while (i--) {
                 const [eventType, objName, idx] = eventList[i],
-                    pkg = typeof objName === 'string' ? getFromCache(objName)?.pkg : objName;
+                    pkg = typeof objName === 'string' ? getPkgByName(objName)?.pkg : objName;
                 
                 if (!pkg) throw new Error(objName + ' required to exist');
                 
                 // Event dispatch, for communication between packages
                 const eventCallback = () => {
-                    objects[eventType][idx] = getObject(objName);
+                    objects[eventType][idx] = getPkgObject(objName);
                     if (--waiting === 0) block?.(objects);
                 };
-                if (pkg._events[eventType]) {
+                if (pkg[eventType]) {
                     eventCallback();
                 } else {
                     (pkg._observers[eventType] ??= []).push(eventCallback);
                     
                     // Load
-                    if (fire(pkg, 'request')) {
-                        const loader = pkg._loader;
-                        
+                    if (!pkg.request) {
+                        pkg.request = true;
+                        const paths = pkg._paths;
                         if (!isLoaded(pkg) && !pkg._source) {
-                            pkg._source = [];
-                            for (let j = 0, len = loader.length; j < len; j++) {
-                                pkg._source[j] = fetchFile(loader[j]);
+                            const source = pkg._source = [];
+                            for (const path of paths) {
+                                const deferred = new Deferred();
+                                source.push(deferred);
+                                fetch(path, {method:'GET'}).then(
+                                    response => {
+                                        if (response.ok) {
+                                            return response.text();
+                                        } else {
+                                            throw new Error('Manifest load error:' + path);
+                                        }
+                                    }
+                                ).then(
+                                    response => {
+                                        deferred.succeed(response + '\n//# sourceURL=' + path);
+                                    }
+                                );
                             }
                         }
                         
-                        const deps = pkg._deps.list,
-                            source = pkg._source ?? [];
-                        let n = loader.length;
-                        
+                        const deps = Array.from(pkg._deps),
+                            source = pkg._source ?? [],
+                            pathsLen = paths.length;
+                        let n = pathsLen;
                         when(
                             {complete:deps}, 
                             () => {
-                                when({complete:deps, load:[pkg]}, () => {fire(pkg, 'complete');});
+                                when(
+                                    {complete:deps, load:[pkg]}, 
+                                    () => {fire(pkg, 'complete');}
+                                );
                                 
                                 if (isLoaded(pkg)) {
                                     fire(pkg, 'load');
@@ -121,9 +111,11 @@
                                             isLoaded(pkg);
                                             fire(pkg, 'load');
                                         } else {
-                                            const index = loader.length - n;
                                             n--;
-                                            loadFile(loader[index], loadNext, source[index]);
+                                            source[pathsLen - 1 - n].callback(code => {
+                                                (new Function(code))();
+                                                loadNext();
+                                            });
                                         }
                                     };
                                     loadNext();
@@ -136,27 +128,20 @@
         },
         
         fire = (pkg, eventType) => {
-            const events = pkg._events,
-                observers = pkg._observers;
-            if (events[eventType]) {
-                return false;
-            } else {
-                events[eventType] = true;
-                const observersForEvent = observers[eventType];
+            if (!pkg[eventType]) {
+                pkg[eventType] = true;
+                const observersForEvent = pkg._observers[eventType];
                 if (observersForEvent) {
-                    delete observers[eventType];
-                    observersForEvent.forEach(callback => {callback();});
+                    delete pkg._observers[eventType];
+                    for (const callback of observersForEvent) callback();
                 }
-                return true;
             }
         },
         
         isLoaded = pkg => {
             if (pkg._isLoaded === undefined) {
-                const names = pkg._names.list;
-                let i = names.length;
-                while (i--) {
-                    if (getObject(names[i]) === undefined) return pkg._isLoaded = false;
+                for (const name of pkg._names) {
+                    if (getPkgObject(name) === undefined) return pkg._isLoaded = false;
                 }
                 return pkg._isLoaded = true;
             } else {
@@ -164,86 +149,29 @@
             }
         },
         
-        Package = function(loader) {
+        Package = function(paths) {
             const self = this,
-                names = self._names = new OrderedSet(),
-                deps = self._deps = new OrderedSet();
+                names = self._names = new Set(),
+                deps = self._deps = new Set();
             
-            self.id = autoIncrement++;
-            self._loader = loader;
-            self._observers = {};
-            self._events = {};
+            self._paths = paths;
+            self._observers = {}; // Event observers.
+            self.request = self.complete = self.load = false; // Events start as false.
             
             // Functions found in manifest files
             self.provides = (...args) => {
-                args.forEach(item => {getFromCache(item).pkg = self;});
-                names.push(...args);
+                for (const arg of args) {
+                    getPkgByName(arg).pkg = self;
+                    names.add(arg);
+                }
                 return self;
             };
-            
             self.requires = (...args) => {
-                deps.push(...args);
+                for (const arg of args) deps.add(arg);
                 return self;
             };
         };
     
-    // File Loader /////////////////////////////////////////////////////////////
-    const HOST_REGEX = /^(https?\:)?\/\/[^\/]+/i,
-        WINDOW_HOST = HOST_REGEX.exec(window.location.href),
-        
-        fetchFile = path => {
-            const pathHost = HOST_REGEX.exec(path);
-            
-            // Don't use browser fetch API for cross-origin requests.
-            if (WINDOW_HOST && (!pathHost || pathHost[0] === WINDOW_HOST[0])) {
-                const source = new Deferred();
-                
-                fetch(path, {method:'GET'}).then(
-                    response => {
-                        if (response.ok) {
-                            return response.text();
-                        } else {
-                            throw new Error('Manifest load error:' + path);
-                        }
-                    }
-                ).then(
-                    response => {
-                        source.succeed(response + '\n//# sourceURL=' + path);
-                    }
-                );
-                
-                return source;
-            }
-        },
-        
-        loadFile = (path, fireCallbacks, source) => {
-            if (source) {
-                // The fetch function will retrieve the code.
-                source.callback(code => {
-                    (new Function(code))();
-                    fireCallbacks();
-                });
-            } else {
-                // Fully qualified URL from another domain so fetch can't retrieve the code. 
-                // We use a script tag instead.
-                let head = document.getElementsByTagName('head')[0],
-                    script = document.createElement('script');
-                
-                script.type = 'text/javascript';
-                script.src = path;
-                script.onload = script.onreadystatechange = () => {
-                    const state = script.readyState;
-                    if (!state || state === 'loaded' || state === 'complete' || (state === 4 && script.status === 200)) {
-                        fireCallbacks();
-                        script.onload = script.onreadystatechange = () => {};
-                        head = script = null;
-                    }
-                };
-                head.appendChild(script);
-            }
-        };
-    
-    // Exports /////////////////////////////////////////////////////////////////
     exports.Packages = manifestFunc => {
         manifestFunc(
             // The "file" function used inside the manifestFunc.
